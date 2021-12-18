25c8213adfa999984ac88e9b0e42139948061e0b
Dmitry Kalinin <x.shreddered.x@gmail.com>
2021-12-18 03:53:52 +0300

    added docs for lab01

--- lab01/include/bresenham.hpp
+++ lab01/include/bresenham.hpp
@@ -3,8 +3,24 @@
 
 #include <opencv2/core.hpp>
 
+/**
+ * Draw a line.
+ *
+ * @param image Grayscale image where a line will be drawn.
+ * @param from Start point.
+ * @param to End point.
+ * @param color Color of a line.
+ */
 void line(cv::Mat image, cv::Point from, cv::Point to, uint8_t color);
 
+/**
+ * Draw a circle.
+ *
+ * @param image Grayscale image where a circle will be drawn.
+ * @param center Center of a circle.
+ * @param radius Radius of a circle.
+ * @paral color Line color.
+ */
 void circle(cv::Mat image, cv::Point center, int radius, uint8_t color);
 
 #endif // INCLUDE_BRESENHAM_HPP_
